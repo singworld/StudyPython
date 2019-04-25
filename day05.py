8bfa703107afeaaffa7455876797f6a8dd95b219
singworld <xiaoxinbian@gmail.com>
2019-04-25 23:26:43 +0800

    modify day05

--- day05.py
+++ day05.py
@@ -7,7 +7,7 @@
 
 for i in range(1,10):
     for j in range(1,i+1):
-        print(f'{j}*{i}',end=' ')
+        print(f'{j}*{i}={j*i}',end=' ')
     print('')
 
 
@@ -54,13 +54,12 @@ for i in range(5,0,-2) :
 # 使用 while 循环一直判断是不是进行
 # 使用 if 语句判断指令类型
 addrBook = {}
-flag = True
 print('|---欢迎进入通讯录程序---|')
 print('|---1、 查询联系人资料---|')
 print('|---2、 插入新的联系人---|')
 print('|---3、 删除已有联系人---|')
 print('|---4、 退出通讯录程序---|')
-while flag:
+while True:
     i = input('请输入指令代码：')
     if '1' == i:
         name = input('请输入联系人姓名：')
@@ -71,13 +70,21 @@ while flag:
             print('该联系人不存在！')
     elif '2' == i:
         name = input('请输入联系人姓名：')
-        phone = input('请输入联系人电话：')
-        addrBook.update({name: phone})
-        print('联系人加入成功！')
+        if name in addrBook.keys():
+            print("该联系人已存在")
+        else:
+            phone = input('请输入联系人电话：')
+            addrBook.update({name: phone})
+            print('联系人加入成功！')
     elif '3' == i:
         name = input('请输入联系人姓名：')
-        addrBook.pop(name)
-        print('联系人删除成功！')
-    else:
+        if name in addrBook.keys():
+            addrBook.pop(name)
+            print('联系人删除成功！')
+        else:
+            print('你要删除的联系人不存在')
+    elif '4' == i:
         print('|---感谢使用通讯录程序---|')
         break
+    else:
+        print('请输入1，2，3，4')
